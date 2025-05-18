481bfaa0690298e13024ff913b7f988bcc31bba8
kamalsushank <kamalsushank@gmail.com>
2025-05-18 22:36:59 +0530

    The third section of projects page .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Hero from "@/components/hero";
-
-
+import Footer from "@/components/footer";
+import TechRev from "@/components/techrev";
+import Body3 from "@/components/body3";
 export default function Home() {
   return (
     <div className="grid grid-rows-[2px_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-4 font-[family-name:var(--font-geist-sans)]">
       {/* Hero Section */}
-      <Hero />
+      {/* <Hero /> */}
+      <Body3 />
+      <TechRev />
+      <Footer />
     </div>
   );
 }
