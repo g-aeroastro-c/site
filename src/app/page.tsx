379a3c110f53e97b358a46fa099d3a92f72216e0
unknown <narayanagupta14@gmail.com>
2025-05-19 10:56:13 +0530

    task/projectsection2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "@/components/hero";
-
+//import Hero from "@/components/hero";
+import ProjectSection2 from "@/components/ProjectSection2";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[2px_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-4 font-[family-name:var(--font-geist-sans)]">
-      {/* Hero Section */}
-      <Hero />
+    <div className="min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+      {/* Project Section */}
+      <ProjectSection2 />
     </div>
   );
 }
